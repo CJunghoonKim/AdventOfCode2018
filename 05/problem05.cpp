3ed332ef4b467f47a0f6af62d95c86845a9d2d05
unknown <cory.j.kim@gmail.com>
2019-01-04 15:58:48 -0500

    Begin problem 05 part I

---
+++ 05/problem05.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+void partOne(string filename);
+void partTwo(string filename);
+
+int main() {
+
+  return 0;
+}
