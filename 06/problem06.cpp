4c0bc1ac8c546cdb1ee0f8377a7b3115dbe2bb22
unknown <cory.j.kim@gmail.com>
2019-01-06 22:55:03 -0500

    Begin part 01

--- 06/problem06.cpp
+++ 06/problem06.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstdlib>
 
 using namespace std;
 
 void partOne(string filename);
 void partTwo(string filename);
+
+int distance(int x1, int y1, int x2, int y2); // Manhattan distance between two points
 
 int main() {
   partOne("input.txt");
@@ -16,9 +19,14 @@ void partOne(string filename) {
   fstream file(filename);
   string line;
   int x, y;
+  char mark = 'A'; // initial marker
 
-  while (file.is_good() && getLine(file, line)) {
+  while (file.is_good() && getline(file, line)) {
 
   }
-  file.close();
+  file.close(); // close file
+}
+
+int distance(int x1, int y1, int x2, int y2) {
+  return abs(x2-x1) + abs(y2-y1);
 }
