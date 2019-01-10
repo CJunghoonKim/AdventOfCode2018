82cda6bbaf5acd9e488ffd5890d4bb067a7e23ec
unknown <cory.j.kim@gmail.com>
2019-01-09 20:16:01 -0500

    Basic radial-expansion (ideal scenario)

--- 06/problem06.cpp
+++ 06/problem06.cpp
@@ -10,13 +10,12 @@ using namespace std;
 void partOne(string filename);
 void partTwo(string filename);
 
-int distance(int x1, int y1, int x2, int y2); // Manhattan distance between two points
-int max(vector<int> &v);                 // find maximum
-void clearGrid(int (&g)[][], int &size); // clear grid
-void radialExpansion(int (&g)[][], int &size, vector<int> &x, vector<int> &y); // perform expansion
+int max(vector<int> &v); // find maximum
+void printGrid(vector<vector<int>> &g); // print grid (debugging purposes)
+void radialExpansion(vector<vector<int>> &g, int &slots, vector<int> &x, vector<int> &y, map<string, int> &rad); // perform expansion
 
 int main() {
-  partOne("input.txt");
+  partOne("test.txt");
   // partTwo("input.txt");
   return 0;
 }
@@ -27,33 +26,35 @@ void partOne(string filename) {
   vector<int> inputX;
   vector<int> inputY;
 
-  while (file.is_good() && getline(file, line)) {
+  while (file.is_open() && getline(file, line)) {
     // store input for pre-processing to determine grid parameters
-    inputX.push_back(atoi(line.substr(0, line.find(','))));
-    inputY.push_back(atoi(line.substr(line.find(','))));
+    inputX.push_back(atoi(line.substr(0, line.find(',')).c_str()));
+    inputY.push_back(atoi(line.substr(line.find(',')+1).c_str()));
   }
   file.close(); // close file
 
-  int grid[max(inputX)][max(inputY)];
-  int mark = 0; // incrementing grid markers
+  int size = max(inputX) > max(inputY) ? max(inputX)+1 : max(inputY)+1;
+  vector<vector<int>> grid(size, vector<int>(size, 0)); // size x size vector initialized with zeroes
+  map<string, int> rad; // source point radii
+  int mark = 1; // incrementing grid markers
   int slots;    // number of free slots left in grid
 
+  // initialize source point radii
+  for (int i = 0; i < inputX.size(); i++) {
+    rad[to_string(inputX[i]) + ", " + to_string(inputY[i])] = 1;
+  }
   // set initial grid state
   for (int i = 0; i < inputX.size(); i++) {
     grid[inputX[i]][inputY[i]] = mark;
     mark++;
   }
-  slots = max(inputX)*max(inputY) - inputX.size();
-  clearGrid(grid, max(inputX));
-  
+  slots = size*size - inputX.size();
+
   // populate grid
   while (slots > 0) {
-    radialExpansion(grid, inputX, inputY);
+    radialExpansion(grid, slots, inputX, inputY, rad);
   }
-}
-
-int distance(int x1, int y1, int x2, int y2) {
-  return abs(x2-x1) + abs(y2-y1);
+  printGrid(grid);
 }
 
 int max(vector<int> &v) {
@@ -66,14 +67,92 @@ int max(vector<int> &v) {
   return res;
 }
 
-void clearGrid(int (&g)[][], int &size) {
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      g[i][j] = 0; // unclaimed
+void printGrid(vector<vector<int>> &g) {
+  for (int i = 0; i < g.size(); i++) {
+    for (int j = 0; j < g[0].size(); j++) {
+      if (g[i][j] == -1) {
+        cout << ". ";
+      } else {
+        cout << g[i][j] << " ";
+      }
     }
+    cout << endl;
   }
 }
 
-void radialExpansion(int (&g)[][], int &size, vector<int> &x, vector<int> &y) {
-
+void radialExpansion(vector<vector<int>> &g, int &slots, vector<int> &x, vector<int> &y, map<string, int> &rad) {
+  int a, b; // corner points
+  vector<string> contest; // contested points
+  /* source points will expand radially w.r.t Manhattan distance in counterclockwise fashion...
+     o . . . A . . . +   --> A-B: f(x, y) = (x--, y++)
+     . . . a x d . . .       B-C: f(x, y) = (x++, y++)
+     . . a x x x d . .       C-D: f(x, y) = (x++, y--)
+     . B x x X x x D .       D-A: f(x, y) = (x--, y--)
+     . . b x x x c . .   special cases arise when corner points A, B, C, D have:
+     . . . b x c . . .       - reached the edge of the grid
+     + . . . C . . . .       - or meet an already-claimed area
+  */
+  for (int i = 0; i < x.size(); i++) {
+    string pair = to_string(x[i]) + ", " + to_string(y[i]);
+    int mark = g[x[i]][y[i]]; // current marker to use
+    // A -> B: x--, y++
+    a = x[i];
+    b = y[i] - rad[pair];
+    for (int j = 0; j < rad[pair]; j++) {
+      if (a-j >= 0 && b+j < g.size() && a-j < g.size() && b+j >= 0) {
+        if (g[a-j][b+j] == 0) { // marks a
+          g[a-j][b+j] = mark;
+        }
+        else if (g[a-j][b+j] == -1) { // neutral ground
+          continue;
+        }
+        // TODO: HANDLE CONTESTED POINTS
+        slots--;
+      }
+    }
+    // B -> C: x++, y++
+    a = x[i] - rad[pair];
+    b = y[i];
+    for (int j = 0; j < rad[pair]; j++) {
+      if (a+j < g.size() && b+j < g.size() && a+j >= 0 && b+j >= 0) {
+        if (g[a+j][b+j] == 0) { // marks b
+          g[a+j][b+j] = mark;
+        }
+        else if (g[a+j][b+j] == -1) { // neutral ground
+          continue;
+        }
+        slots--;
+      }
+    }
+    // C -> D: x++, y--
+    a = x[i];
+    b = y[i] + rad[pair];
+    for (int j = 0; j < rad[pair]; j++) {
+      if (a+j < g.size() && b-j >= 0 && a+j >= 0 && b-j < g.size()) {
+        if (g[a+j][b-j] == 0) {// marks c
+          g[a+j][b-j] = mark;
+        }
+        else if (g[a+j][b-j] == -1) { // neutral ground
+          continue;
+        }
+        slots--;
+      }
+    }
+    // D -> A: x--, y--
+    a = x[i] + rad[pair];
+    b = y[i];
+    for (int j = 0; j < rad[pair]; j++) {
+      if (a-j >= 0 && b-j >= 0 && a-j < g.size() && b-j < g.size()) {
+        if (g[a-j][b-j] == 0) { // marks d
+          g[a-j][b-j] = mark;
+        }
+        else if (g[a-j][b-j] == -1) { // neutral ground
+          continue;
+        }
+        slots--;
+      }
+    }
+    // update radius
+    rad[pair]++;
+  }
 }
